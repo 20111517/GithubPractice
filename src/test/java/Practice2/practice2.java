7e8a009304a7020bef49bc38e3eac632baa2ce80
Esonic <tariksahin085@gmail.com>
2022-11-12 10:38:14 +0300

    fourth commit

--- src/test/java/Practice2/practice2.java
+++ src/test/java/Practice2/practice2.java
@@ -3,6 +3,11 @@ package Practice2;
 public class practice2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-    }
+        String day = "saturday";
+        if (day == "saturday") {
+            System.out.println("this is the sixth day of the week");
 
+        }
+
+    }
 }
